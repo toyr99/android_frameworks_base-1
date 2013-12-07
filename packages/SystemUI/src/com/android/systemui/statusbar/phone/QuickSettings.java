ee9f10e5876c2c45984e8c0181a992b5d3af3d80
Carlo Savignano <stevewatersy@gmail.com>
2013-12-07 20:18:23 +0100

    Quicksettings: Immersive: Fix collapse panel animation.
    Right now it collapse after the ui goes into immersive mode so the collapse animation is not showed.
    First collapse the panel then go into immersive mode so we see the animation.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: Icf2925fa517fc077929e02d1f96a8cd51163b809

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -686,6 +686,7 @@ class QuickSettings {
                     immersiveTile.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            collapsePanels();
                             boolean immersiveModeOn = Settings.System.getInt(mContext
                                     .getContentResolver(), Settings.System.IMMERSIVE_MODE, 0) == 1;
                             immersiveTile.setImageResource(immersiveModeOn
@@ -696,7 +697,6 @@ class QuickSettings {
                                             R.string.quick_settings_immersive_mode_label);
                             Settings.System.putInt(mContext.getContentResolver(),
                                     Settings.System.IMMERSIVE_MODE, immersiveModeOn ? 0 : 1);
-                            collapsePanels();
                         }
                     });
                     parent.addView(immersiveTile);
