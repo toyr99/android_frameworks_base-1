140bb29aed0c97e358910542c0b2167e3fe82faa
Kun Liang <kunliang@codeaurora.org>
2013-12-16 17:33:28 +0800

    Squashed commit of QuickBoot feature from Qualcomm
    QuickBoot: don't log boot events when in QuickBoot mode.
    
    Don't need log boot events when booting from QuickBoot mode.
    
    Change-Id: Id381530418c09ac1d650f45e7a368dbd7945aff3
    
    QuickBoot: don't wake up screen when usb connected
    
    Don't wake up screen in QuickBoot mode when usb connected.
    
    Change-Id: If65936a0edbbc3b36a0768294e5c3bd7d741018a
    
    QuickBoot: filt out alarms that for non-system users
    
    Don't allow alarms for non-system users when under QUickBoot
    mode.
    
    Change-Id: I322d08f7236b6895462c4cd3304326c66f67ddd2
    
    QuickBoot: restrict app's auto start under Quickboot mode
    
    Don't let broadcast trigger apps auto start when in Quickboot mode.
    
    Change-Id: I5d156cbea4ef18397ce4569cb74f73a446a6e49b
    
    QuickBoot: remove alarms for killed apps
    
    Device may be woken up by WAKEUP alarms, which have been set
    by those killed apps before. Here to get the killed apps list
    and remove the alarms from AlarmManagerService.
    
    Change-Id: Ib34f5fc234b834b2fe4528229c267c8693fc76cb
    
    SyncManager: Fix SyncManager null pointer issue under QuickBoot
    
    BOOT_COMPLETED broadcast will cause mBootQueue null pointer issue.
    Then system_server will crash. Add protection here to avoid
    system_server crash.
    
    Change-Id: I78029dfa90709455e4c2220e06b95640383655f8
    
    QuickBoot: add execption restore mechanism
    
    There may be some exceptions like battery unplugged When device
    is put into QuickBoot mode. Then device will be in airplane
    mode after next boot. We don't want such kind of things happen.
    So start restore task to check if need restore before main
    screen shows.
    
    Change-Id: I3d0967c2f446fd71eca87ff085617edca549c984
    
    QuickBoot: switch normal power off to quickboot
    
    Handle power off button when quickboot mode enabled.
    
    Change-Id: I6a668cbe823018b5e508633a76202d0df0381694
    
    QuickBoot: support QuickBoot power on
    
    Support long press power key to power on when in QuickBoot mode.
    
    Change-Id: Id493d352bce73683b9696a2685a6c902f1195799
    
    QuickBoot: Don't handle SHUTDOWN broadcast from QuickBoot
    
    Don't process the ACTION_SHUTDOWN broadcast sent by QuickBoot.
    It is only for apps.
    Change-Id: I73d1a7b8edda0a898dd052831033806e18b37c99
    
    QuickBoot: rename some intents and broadcasts
    
    Modify some improper names for intents or broadcasts of QuickBoot.
    
    Change-Id: I92357b0110643bb425199452cb03bd75248a5d83
    
    QuickBoot: Support poweroff alarm under QuickBoot mode
    
    Add white list in QuickBoot mode to support poweroff alarm.
        1. Allow original deskclock app and quickboot app to be launched.
        2. Don't filter out deskclock's alarms.
    
    Change-Id: Ie4254e3da45837d3185d3daba625a42c50fd7f52
    
    QuickBoot: enable poweroff charging under QuickBoot mode
    
    control native charger's enable/disable under QuickBoot
    mode.
    
    Change-Id: I4f80c74b042e03bddc9f7fe419f4ed1f8b1409d5
    
    QuickBoot: Add wakeup check under QuickBoot mode
    
    We only want QuickBoot application to wakeup screen in QuickBoot
    mode. Add wakeup check to filter out other wakeup callers.
    
    Change-Id: Id82a0938003d2a776efda0e39a57877f41fc1ee4

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -26,6 +26,7 @@ import com.android.internal.R;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -270,6 +271,9 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mItems = new ArrayList<Action>();
 
+        final boolean quickbootEnabled = Settings.System.getInt(
+                mContext.getContentResolver(), "enable_quickboot", 0) == 1;
+
         // first: power off
         mItems.add(
             new SinglePressAction(
@@ -277,6 +281,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     R.string.global_action_power_off) {
 
                 public void onPress() {
+                    // goto quickboot mode
+                    if (quickbootEnabled) {
+                        startQuickBoot();
+                        return;
+                    }
                     // shutdown by making sure radio and power are handled accordingly.
                     mWindowManagerFuncs.shutdown(true);
                 }
@@ -1119,6 +1128,16 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         mContext.sendBroadcastAsUser(intent, UserHandle.ALL);
         if (!mHasTelephony) {
             mAirplaneState = on ? ToggleAction.State.On : ToggleAction.State.Off;
+        }
+    }
+
+    private void startQuickBoot() {
+
+        Intent intent = new Intent("org.codeaurora.action.QUICKBOOT");
+        intent.putExtra("mode", 0);
+        try {
+            mContext.startActivityAsUser(intent,UserHandle.CURRENT);
+        } catch (ActivityNotFoundException e) {
         }
     }
 
