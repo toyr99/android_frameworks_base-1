7a658f9e4844ba179953635f65b86851a5691172
snak3ater <saeedyasir91@gmail.com>
2014-03-09 03:11:59 +0500

    Frameworks: port statusbar notification count (1/2)
    Change-Id: Id23ac43866fa7fb9043d1f598a1f88cd4fa9ab9b
    
    update notification count backgrounds and text color to match kitkat ui
    
      - also add duplicates in inverted folders to keep inline with trds themeing
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2852,6 +2852,12 @@ public final class Settings {
         public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
 
         /**
+         * Show the pending notification counts as overlays on the statusbar
+         * @hide
+         */
+        public static final String STATUSBAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
