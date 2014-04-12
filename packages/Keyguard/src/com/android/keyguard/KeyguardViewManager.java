abd85cb3599ae22a5f458e629588810aab9dde03
AndroidRul3z <ferrettidario@gmail.com>
2014-04-12 19:08:12 +0200

    Don't recreate keyguard for MCC/MNC changes.
    https://github.com/CyanogenMod/android_frameworks_base/commit/8f3abc06fc9d62734f40a2eb1cf69fe5f137ed0b
    
    Fix
    
    ...

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -169,6 +169,7 @@ public class KeyguardViewManager {
 
         private Drawable mUserBackground;
         private Drawable mCustomBackground;
+        private Configuration mLastConfiguration;
         private int mLockscreenStyle;
 
         // This is a faster way to draw the background on devices without hardware acceleration
@@ -198,6 +199,7 @@ public class KeyguardViewManager {
             super(context);
             setBackground(mBackgroundDrawable);
             cacheUserImage();
+            mLastConfiguration = new Configuration(context.getResources().getConfiguration());
 
             context.registerReceiver(new BroadcastReceiver() {
                 @Override
@@ -298,12 +300,16 @@ public class KeyguardViewManager {
         @Override
         protected void onConfigurationChanged(Configuration newConfig) {
             super.onConfigurationChanged(newConfig);
-            if (mKeyguardHost.getVisibility() == View.VISIBLE) {
+            int diff = newConfig.diff(mLastConfiguration);
+            if ((diff & ~(ActivityInfo.CONFIG_MCC | ActivityInfo.CONFIG_MNC)) == 0) {
+                if (DEBUG) Log.v(TAG, "onConfigurationChanged: no relevant changes");
+            } else if (mKeyguardHost.getVisibility() == View.VISIBLE) {
                 // only propagate configuration messages if we're currently showing
                 maybeCreateKeyguardLocked(shouldEnableScreenRotation(), true, null);
             } else {
                 if (DEBUG) Log.v(TAG, "onConfigurationChanged: view not visible");
             }
+            mLastConfiguration = new Configuration(newConfig);
         }
 
         @Override
