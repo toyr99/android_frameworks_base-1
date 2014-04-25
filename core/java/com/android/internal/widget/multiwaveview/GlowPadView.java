5f4935d847c9ca67e9f55b8d634fd9b46e218b13
Martin Rodemann <martin@rodemann.org>
2014-04-25 14:06:17 +0200

    Fix fc in NavRing Target Settings When greenify or tasker is installed and their accessibility-service active, Navring Target Settings failed because of an out of Index Exception, this commit fixes this

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -983,7 +983,7 @@ public class GlowPadView extends View {
                 }
                 if (AccessibilityManager.getInstance(mContext).isEnabled()) {
                     String targetContentDescription = getTargetDescription(activeTarget);
-                    announceForAccessibility(targetContentDescription);
+                    if (targetContentDescription != null && targetContentDescription !="") announceForAccessibility(targetContentDescription);
                 }
             }
         }
@@ -1318,7 +1318,11 @@ public class GlowPadView extends View {
                 return null;
             }
         }
-        return mTargetDescriptions.get(index);
+        if (mTargetDescriptions.size()>index) {
+            return mTargetDescriptions.get(index);
+        } else {
+            return null;
+        }
     }
 
     private String getDirectionDescription(int index) {
@@ -1333,7 +1337,11 @@ public class GlowPadView extends View {
                 return null;
             }
         }
-        return mDirectionDescriptions.get(index);
+        if (mDirectionDescriptions.size()>index) {
+            return mDirectionDescriptions.get(index);
+        } else {
+            return null;
+        }
     }
 
     private ArrayList<String> loadDescriptions(int resourceId) {
