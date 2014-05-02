0ef565fd8c6becbaf476fe2d2f33d35bb34b18eb
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-02 12:19:26 -0300

    PEEK: configurable pickup timeout (1/2)
    - Allow the user to set different pickup timeout when a notification comes up through PEEK.
    
    Change-Id: Ifcd2f13101dcf6973ebcf016bce5f5314e1ce04c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2816,6 +2816,12 @@ public final class Settings {
          */
         public static final String PEEK_STATE = "peek_state";
 
+	/**
+         * Set the timeout of peek when pikcing up the device
+         * @hide
+         */
+        public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
+
         /**
          * In call dialpad state.
          * 0 = hidden
