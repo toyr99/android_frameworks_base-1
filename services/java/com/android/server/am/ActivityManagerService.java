ef281f8649db39d89d1e42531294e599016094c3
Clark Scheff <clark@cyngn.com>
2014-05-06 08:32:54 -0700

    CM11 Themes: Ensure customTheme is not null.
    The configuration retrieved via getConfiguration() sometimes contains
    a null customTheme which results in the theme returning to holo when
    applying a component such as icons.  Icons will change but styles
    and fonts would return to the default.
    
    Change-Id: Iac0cb8eed5de8fdc9fe8efd697d968f26e801c08

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -14029,6 +14029,9 @@ public final class ActivityManagerService extends ActivityManagerNative
         Configuration ci;
         synchronized(this) {
             ci = new Configuration(mConfiguration);
+            if (ci.customTheme == null) {
+                ci.customTheme = CustomTheme.getBootTheme(mContext.getContentResolver());
+            }
         }
         return ci;
     }
