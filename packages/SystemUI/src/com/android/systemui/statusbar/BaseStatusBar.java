9b7bfc668b4cda6671463126da3085759a18d381
BigBrother1984 <stevewatersy@gmail.com>
2014-05-24 05:39:56 +0200

    [1/2] Prepare NotificationManagerService for new format
    * Needed for blacklisting
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I7801d65016aa949a5e2406e00871229a602829b9
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar;
 
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
+import android.app.INotificationManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
@@ -109,6 +110,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     public static final int EXPANDED_FULL_OPEN = -10001;
 
     protected CommandQueue mCommandQueue;
+    protected INotificationManager mNotificationManager;
     protected IStatusBarService mBarService;
     protected H mHandler = createHandler();
 
@@ -159,6 +161,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected int mImmersiveModeStyle;
 
     private RecentsComponent mRecents;
+
+    public INotificationManager getNotificationManager() {
+        return mNotificationManager;
+    }
 
     public IStatusBarService getStatusBarService() {
         return mBarService;
@@ -266,6 +272,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mNotificationManager = INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 
         mProvisioningObserver.onChange(false); // set up
         mContext.getContentResolver().registerContentObserver(
