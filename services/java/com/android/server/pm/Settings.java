CyanogenMod/android_frameworks_base

Install themes and icon packs for secondary users.
Change-Id: I48ae6835223510a433642fced5b96b6c3954baf6

services/java/com/android/server/pm/Settings.java
@@ -2714,8 +2714,10 @@ final class Settings {
         FileUtils.setPermissions(path.toString(), FileUtils.S_IRWXU | FileUtils.S_IRWXG
                 | FileUtils.S_IXOTH, -1, -1);
         for (PackageSetting ps : mPackages.values()) {
+            boolean installed = ((ps.pkgFlags&ApplicationInfo.FLAG_SYSTEM) != 0) ||
+                    (ps.pkg.mIsThemeApk || ps.pkg.mIsLegacyThemeApk || ps.pkg.mIsLegacyThemeApk);
             // Only system apps are initially installed.
-            ps.setInstalled((ps.pkgFlags&ApplicationInfo.FLAG_SYSTEM) != 0, userHandle);
+            ps.setInstalled(installed, userHandle);
             // Need to create a data directory for all apps under this user.
             installer.createUserData(ps.name,
                     UserHandle.getUid(userHandle, ps.appId), userHandle,

toyr99/android_frameworks_base-1

services/java/com/android/server/pm/Settings.java
@@ -2675,8 +2675,10 @@ final class Settings {
         FileUtils.setPermissions(path.toString(), FileUtils.S_IRWXU | FileUtils.S_IRWXG
                 | FileUtils.S_IXOTH, -1, -1);
         for (PackageSetting ps : mPackages.values()) {
+            boolean installed = ((ps.pkgFlags&ApplicationInfo.FLAG_SYSTEM) != 0) ||
+                    (ps.pkg.mIsThemeApk || ps.pkg.mIsLegacyThemeApk || ps.pkg.mIsLegacyIconPackApk);
             // Only system apps are initially installed.
-            ps.setInstalled((ps.pkgFlags&ApplicationInfo.FLAG_SYSTEM) != 0, userHandle);
+            ps.setInstalled(installed, userHandle);
             // Need to create a data directory for all apps under this user.
             installer.createUserData(ps.name,
                     UserHandle.getUid(userHandle, ps.appId), userHandle);