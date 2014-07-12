e10ef9d9818de63ff31bbaa799f1f78206d34731
snak3ater <saeedyasir91@gmail.com>
2014-07-12 13:10:46 +0500

    [2/2] frameworks/base: Enable quick unlock
    Automatically unlock the lockscreen when the correct pin/password is keyed.
    
    Change-Id: I3c1df4b573321a9343d1c339055577d5ca737f14
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2839,6 +2839,12 @@ public final class Settings {
          */
         public static final String CALL_UI_IN_BACKGROUND = "call_ui_in_background";
 
+	  /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
