wanginator/android_frameworks_base

Base:Quiet Hours[1/2]
based on cm && based on Omni's adaptation & AOSPA

PS3: Whitespace fixups
PS4: fix imports for QHHelper
PS5: We don't have battery LED enabled

Conflicts:
	services/java/com/android/server/NotificationManagerService.java

core/java/com/android/internal/util/rascarlo/QuietHoursHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2013 SlimRoms Project
+ * Copyright (C) 2014 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.rascarlo;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.util.Calendar;
+
+public class QuietHoursHelper {
+
+    public static boolean inQuietHours(Context context, String option) {
+        boolean mode = true;
+        boolean quietHoursEnabled = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0,
+                UserHandle.USER_CURRENT_OR_SELF) != 0;
+        int quietHoursStart = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_START, 0,
+                UserHandle.USER_CURRENT_OR_SELF);
+        int quietHoursEnd = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_END, 0,
+                UserHandle.USER_CURRENT_OR_SELF);
+
+        if (option != null) {
+            mode = Settings.System.getIntForUser(context.getContentResolver(),
+                    option, 0,
+                    UserHandle.USER_CURRENT_OR_SELF) != 0;
+        }
+
+        if (quietHoursEnabled && mode) {
+            // 24-hours toggleable
+            if (quietHoursStart == quietHoursEnd) {
+                return true;
+            }
+            // Get the date in "quiet hours" format
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
+        }
+        return false;
+    }
+}

services/java/com/android/server/VibratorService.java
@@ -44,6 +44,7 @@ import android.view.InputDevice;
 
 import com.android.internal.app.IAppOpsService;
 import com.android.internal.app.IBatteryStats;
+import com.android.internal.util.rascarlo.QuietHoursHelper;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -201,7 +202,8 @@ public class VibratorService extends IVibratorService.Stub
         // timeout of 0 or negative. This will ensure that a vibration has
         // either a timeout of > 0 or a non-null pattern.
         if (milliseconds <= 0 || (mCurrentVibration != null
-                && mCurrentVibration.hasLongerTimeout(milliseconds))) {
+                && mCurrentVibration.hasLongerTimeout(milliseconds))
+                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_HAPTIC)) {
             // Ignore this vibration since the current vibration will play for
             // longer than milliseconds.
             return;
@@ -237,6 +239,9 @@ public class VibratorService extends IVibratorService.Stub
         if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.VIBRATE)
                 != PackageManager.PERMISSION_GRANTED) {
             throw new SecurityException("Requires VIBRATE permission");
+        }
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_HAPTIC)) {
+            return;
         }
         verifyIncomingUid(uid);
         // so wakelock calls will succeed

toyr99/android_frameworks_base-1

	core/java/android/provider/Settings.java

core/java/com/android/internal/util/simpleaosp/QuietHoursHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2013 SlimRoms Project
+ * Copyright (C) 2014 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.simpleaosp;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.util.Calendar;
+
+public class QuietHoursHelper {
+
+    public static boolean inQuietHours(Context context, String option) {
+        boolean mode = true;
+        boolean quietHoursEnabled = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0,
+                UserHandle.USER_CURRENT_OR_SELF) != 0;
+        int quietHoursStart = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_START, 0,
+                UserHandle.USER_CURRENT_OR_SELF);
+        int quietHoursEnd = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_END, 0,
+                UserHandle.USER_CURRENT_OR_SELF);
+
+        if (option != null) {
+            mode = Settings.System.getIntForUser(context.getContentResolver(),
+                    option, 0,
+                    UserHandle.USER_CURRENT_OR_SELF) != 0;
+        }
+
+        if (quietHoursEnabled && mode) {
+            // 24-hours toggleable
+            if (quietHoursStart == quietHoursEnd) {
+                return true;
+            }
+            // Get the date in "quiet hours" format
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
+        }
+        return false;
+    }
+}

services/java/com/android/server/VibratorService.java
@@ -44,6 +44,7 @@ import android.view.InputDevice;
 
 import com.android.internal.app.IAppOpsService;
 import com.android.internal.app.IBatteryStats;
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -201,7 +202,8 @@ public class VibratorService extends IVibratorService.Stub
         // timeout of 0 or negative. This will ensure that a vibration has
         // either a timeout of > 0 or a non-null pattern.
         if (milliseconds <= 0 || (mCurrentVibration != null
-                && mCurrentVibration.hasLongerTimeout(milliseconds))) {
+                && mCurrentVibration.hasLongerTimeout(milliseconds))
+                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_HAPTIC)) {
             // Ignore this vibration since the current vibration will play for
             // longer than milliseconds.
             return;
@@ -237,6 +239,9 @@ public class VibratorService extends IVibratorService.Stub
         if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.VIBRATE)
                 != PackageManager.PERMISSION_GRANTED) {
             throw new SecurityException("Requires VIBRATE permission");
+        }
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_HAPTIC)) {
+            return;
         }
         verifyIncomingUid(uid);
         // so wakelock calls will succeed